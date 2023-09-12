6cfdba02f858f56d90150298da7b9ed55a2c3cdf
Andrey A. Ugolnik <andrey@ugolnik.info>
2023-09-12 15:21:23 +0200

    Config updated.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,13 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- Maximize window at startup
+wezterm.on("gui-startup", function()
+	local mux = wezterm.mux
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -12,12 +19,36 @@ end
 
 -- This is where you actually apply your config choices
 
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 0,
+}
+
+-- Disable the title bar but enable the resizable border
+config.window_decorations = "RESIZE"
+
+-- When there is only a single tab, the tab bar is hidden
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18.0
+
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", style = "Normal" }),
+	},
+	{
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font("JetBrains Mono", { weight = "Bold", style = "Italic" }),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
